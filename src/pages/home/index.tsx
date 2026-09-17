0a3552378815cb52882c2039e22336360bd9aac8
Guido Stocco <guidostoccodev@gmail.com>
2026-09-16 21:09:56 -0300

    creating homescreen

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
-
+import { Container } from "../../components/container";
+import bwmImg from './../../assets/bmw.png'
 
 
 
@@ -11,9 +12,85 @@
 
 export function Home() {
   return(
-    <div>
-      <h1>Tela home</h1>
-    </div>
+    <Container>
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+        <input
+        className="w-full border-1 rounded-lg h-9 px-3 border-gray-300"
+          placeholder="Digite o nome do carro..."
+        />
+
+        <button 
+          className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
+        >
+          Buscar
+        </button>
+      </section>
+
+      <h1
+        className="font-bold text-center mt-6 mb-4 text-2xl"
+      >Carros novos e usados em todo o brasil</h1>
+
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <section className="w-full bg-white rounded-lg">
+          <img
+            src={bwmImg}
+            alt="Carro"
+            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+          />
+          <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
+          <div className="flex flex-col px-2">
+              <span className="text-zinc-700 mb-6">ano 2021/2020 / 50.822 km</span>
+              <strong className="text-black font-medium text-xl">R$ 193.900</strong>
+          </div>
+
+          <div className="w-full h-px bg-slate-200 my-2"></div>
+
+          <div className="px-2 pb-2 ">
+            <span className="text-zinc-700">Brasília - DF</span>
+          </div>
+        </section>
+
+         <section className="w-full bg-white rounded-lg">
+          <img
+            src={bwmImg}
+            alt="Carro"
+            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+          />
+          <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
+          <div className="flex flex-col px-2">
+              <span className="text-zinc-700 mb-6">ano 2021/2020 / 50.822 km</span>
+              <strong className="text-black font-medium text-xl">R$ 193.900</strong>
+          </div>
+
+          <div className="w-full h-px bg-slate-200 my-2"></div>
+
+          <div className="px-2 pb-2 ">
+            <span className="text-zinc-700">Brasília - DF</span>
+          </div>
+        </section>
+
+         <section className="w-full bg-white rounded-lg">
+          <img
+            src={bwmImg}
+            alt="Carro"
+            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+          />
+          <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
+          <div className="flex flex-col px-2">
+              <span className="text-zinc-700 mb-6">ano 2021/2020 / 50.822 km</span>
+              <strong className="text-black font-medium text-xl">R$ 193.900</strong>
+          </div>
+
+          <div className="w-full h-px bg-slate-200 my-2"></div>
+
+          <div className="px-2 pb-2 ">
+            <span className="text-zinc-700">Brasília - DF</span>
+          </div>
+        </section>
+
+      </main>
+
+    </Container>
   )
 }
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,27 @@
-
+import { Link } from 'react-router-dom'
+import { Container } from '../../components/container'
+import logoImg from './../../assets/logo.svg'
 
 
 export function Login() {
   return(
-    <div>
-      <h1>Tela Login</h1>
-    </div>
+    <Container>
+      <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
+        <Link to='/' className='max-w-sm w-full mb-6'>
+          <img
+            src={logoImg}
+            alt='Logo do site'
+            className='w-full'
+          />
+        </Link>
+
+        <form className='bg-white w-full max-w-xl rounded-xl'>
+            <input
+
+            />
+        </form>
+      </div>
+    </Container>
   )
 }
 
